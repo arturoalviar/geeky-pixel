00fa7dcbda039696f58418733390047691bf2627
arturoalviar <arturoalviar@gmail.com>
2015-10-03 23:27:14 -0700

    setup gulp, added basic html structure

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp      = require('gulp');
+var scss      = require('gulp-sass');
+var cssMin    = require('gulp-minify-css');
+var haml      = require('gulp-haml');
+
+
+gulp.task('sassy', function(){
+  return gulp.src('./scss/**/*.scss')
+    .pipe(scss())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('minify', ['sassy'] , function(){
+  return gulp.src('./css/style.css')
+    .pipe(cssMin())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('hammy', function(){
+  gulp.src('./index.haml')
+    .pipe(haml())
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('build', ['minify', 'hammy']);
+
+gulp.task('default', ['build']);
