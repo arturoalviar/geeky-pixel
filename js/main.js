b8303f8cce9bb704f644a4181ca594c452a3e4db
arturoalviar <arturoalviar@gmail.com>
2015-10-21 20:36:22 -0700

    Added javascript, improved typograpgy

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+
 +function($){
   'use strict';
+
+    var $root = $('html, body');
+    var $input = $('.input');
+    var $main = $('.main');
+    var $header = $('.site-header');
+    var $bannerHeight = $('.banner').outerHeight();
+
+    $input.blur(function(){
+        var $this = $(this);
+        $this.val() ? $this.addClass('active-label') : $this.removeClass('active-label');
+    });
+
+    console.log($bannerHeight);
+
+    $(window).on('scroll' , {"prevScroll": 0 },function(){
+      var $currScroll = $(window).scrollTop();
+      if($currScroll > $bannerHeight){
+          $header.addClass('site-header-fixed');
+          $main.addClass('main-top-margin');
+      }
+
+      else if($currScroll < $bannerHeight){
+        $header.removeClass('site-header-fixed');
+        $main.removeClass('main-top-margin');
+      }
+      this.prevScroll = $currScroll;
+    });
+
 }($);
